0ddcfa49db57c6cd60501fbd44d01b1e3eff76db
Brant Porter and Zach Beecher <brant@>
2017-02-15 10:18:09 -0800

    user input turns to uppercase

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(function() {
-  $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var placeInput = $("input#place").val();
+  $("form#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val().toUpperCase();
+    var person2Input = $("input#person2").val().toUpperCase();
+    var animalInput = $("input#animal").val().toUpperCase();
+    var exclamationInput = $("input#exclamation").val().toUpperCase();
+    var verbInput = $("input#verb").val().toUpperCase();
+    var placeInput = $("input#place").val().toUpperCase();
 
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
@@ -13,7 +13,9 @@ $(function() {
     $(".exclamation").text(exclamationInput);
     $(".verb").text(verbInput);
     $(".place").text(placeInput);
+    // $("#story").show();
     event.preventDefault();
     $("#story").show();
+
   });
 });
